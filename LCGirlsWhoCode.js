7f365e28f36e657894a4e0a670f2ecb1a9875cf4
melissa <lisayao43@gmail.com>
2020-10-06 16:31:36 -0600

    added snap scroll

---
+++ LCGirlsWhoCode.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var stickyNavTop = $('.navbar').offset().top;
+    var stickyNav = function(){
+     var scrollTop = $(window).scrollTop(); 
+     if (scrollTop > stickyNavTop) { 
+         $('.navbar').addClass('sticky');
+     } else {
+         $('.navbar').removeClass('sticky'); 
+     }
+ };
+    stickyNav();
+    $(window).scroll(function(){
+        stickyNav();
+    });
+});
